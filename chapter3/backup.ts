fd1dc5ad68d8455967dfdd041560bcfbf629f180
kiethohoai <kietho.060193@gmail.com>
2024-06-14 16:44:59 +0700

    21 - TypeScript Switch Case

---
+++ chapter3/backup.ts
@@ -0,0 +1,7 @@
+////////////// #20 TypeScript If else ///////////////
+let age: number = 30;
+if (age > 18) {
+    console.log("Your Good Age!");
+} else {
+    console.log("Your Bad Age!");
+}
--- chapter3/chapter3.ts
+++ chapter3/chapter3.ts
@@ -1,7 +1,16 @@
-////////////// #20 TypeScript If else ///////////////
-let age: number = 30;
-if (age > 18) {
-    console.log("Your Good Age!");
-} else {
-    console.log("Your Bad Age!");
+////////////// #21 TypeScript Switch Case ///////////////
+const myAge: number = 30;
+
+switch (myAge) {
+    case 20:
+        console.log("You're 20 years old!");
+        break;
+    case 25:
+        console.log("You're 25 years old!");
+        break;
+    case 30:
+        console.log("You're 30 years old!");
+        break;
+    default:
+        console.log("How old are you?");
 }
