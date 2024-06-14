41d1a433f48b2f5162e4cfb60c98f1b4965cd68c
kiethohoai <kietho.060193@gmail.com>
2024-06-14 12:59:50 +0700

    13 - Tuple Types

--- projects/lesson13/lesson13.ts
+++ projects/lesson13/lesson13.ts
@@ -1,12 +1,23 @@
-let skills: (string | number)[] = ['Hỏi Dân IT', 25];
-skills.push('coding');
-skills.push(69);
+// let skills: (string | number)[] = ['Hỏi Dân IT', 25];
+// skills.push('coding');
+// skills.push(69);
+// //tuple: dataType/size/order
+// let skills2: [string, number] = ['Hỏi Dân IT', 25];
+// let skills3: [boolean, string, number?];
+// skills3 = [true, 'Eric'];
 
-//tuple: dataType/size/order
-let skills2: [string, number] = ['Hỏi Dân IT', 25];
+// let infor: (string | number)[] = ["Kiet", 30];
+// console.log("🚀CHECK  infor =>", infor);
 
-let skills3: [boolean, string, number?];
+// let address: (string | number)[] = ["30 Ha Huy Tap", 43];
+// console.log("🚀CHECK  address =>", address);
 
-skills3 = [true, 'Eric'];
+// let data: [string, number, boolean];
+// data = ["Ho Hoai Kiet", 30, true];
+// console.log("🚀CHECK  data =>", data);
 
+// let data2: (string | number | boolean)[] = [30, true, "Ho Hoai Kiet"];
+// console.log("🚀CHECK  data2 =>", data2);
 
+let data3: [string, number?, boolean?] = ["DavidHo"];
+console.log("🚀CHECK  data3 =>", data3);
