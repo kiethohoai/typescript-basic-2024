72011a39ab52982d582ca52adb42aaea22ee531d
kiethohoai <kietho.060193@gmail.com>
2024-06-14 15:28:13 +0700

    18 - TypeScript Union Type

--- projects/lesson18/lesson18.ts
+++ projects/lesson18/lesson18.ts
@@ -1,14 +1,26 @@
-function addNumberOrString(a: number | string | object | boolean, b: number | string) {
-    if (typeof a === 'number' && typeof b === 'number') {
+// function addNumberOrString(a: number | string | object | boolean, b: number | string) {
+//     if (typeof a === 'number' && typeof b === 'number') {
+//         return a + b;
+//     }
+//     if (typeof a === 'string' && typeof b === 'string') {
+//         return a.concat(b);
+//     }
+//     throw new Error('Parameters must be numbers or strings');
+// }
+
+// //loi khi running
+// //loi khi compile
+// console.log(">>> check: ", addNumberOrString(true, "Hoi dan it"))
+
+function addNumberOrString(a: number | string, b: number | string) {
+    if (typeof a === "number" && typeof b === "number") {
         return a + b;
     }
-    if (typeof a === 'string' && typeof b === 'string') {
+    if (typeof a === "string" && typeof b === "string") {
         return a.concat(b);
     }
-    throw new Error('Parameters must be numbers or strings');
+    throw new Error("Parameters must be numbers or strings");
 }
 
-//loi khi running
-//loi khi compile
-console.log(">>> check: ", addNumberOrString(true, "Hoi dan it"))
-
+let temp = addNumberOrString(1, "hai");
+console.log("🚀CHECK  temp =>", temp);
