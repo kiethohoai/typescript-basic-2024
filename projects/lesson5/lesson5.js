ff640d92d3992c45c93b92dce413d5900215abf7
kiethohoai <kietho.060193@gmail.com>
2024-06-14 08:13:29 +0700

    05 - TypeScript Types

--- projects/lesson5/lesson5.js
+++ projects/lesson5/lesson5.js
@@ -1,4 +1,5 @@
 const name = 'Eric';
+console.log("🚀CHECK  name.length =>", name.length);
 
-console.log("length = ", name.length)
-console.log('upPerCasse = ', name.toUpperCase())
+// console.log("length = ", name.length)
+// console.log('upPerCasse = ', name.toUpperCase())
--- projects/lesson5/lesson5.ts
+++ projects/lesson5/lesson5.ts
@@ -1,6 +1,6 @@
-const nameTS1 = 'Eric';
+const myName = "hohoaikiet";
+console.log("🚀CHECK  myName.length =>", myName.length);
+console.log("🚀CHECK  myName.toUpperCase() =>", myName.toUpperCase());
 
-console.log('length = ', nameTS1.length)
-
-console.log('upPerCasse = ', nameTS1.toUpperCase())
-
+// console.log('length = ', nameTS1.length)
+// console.log('upPerCasse = ', nameTS1.toUpperCase())
