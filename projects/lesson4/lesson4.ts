5faacb617639f04ecb39269e0ac0b875b06e265a
kiethohoai <kietho.060193@gmail.com>
2024-06-14 07:57:15 +0700

    04 - Why TypeScript

--- projects/lesson4/lesson4.ts
+++ projects/lesson4/lesson4.ts
@@ -3,8 +3,8 @@ const sum3 = (x: number, y: number) => {
 }
 
 //no error
-// console.log(">>> check sum3 = ", sum3(5, 10)); //15
+console.log(">>> check sum3 = ", sum3(5, 10)); //15
 
 
 //error
-console.log(">>> check sum3 = ", sum3(5, '10')); //15
+// console.log(">>> check sum3 = ", sum3(5, '10')); //15
