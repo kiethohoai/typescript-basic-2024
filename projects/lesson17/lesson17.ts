e54c6978592187466d86de761c9dee2d7747f016
kiethohoai <kietho.060193@gmail.com>
2024-06-14 14:56:09 +0700

    17 - TypeScript Data Type - Never

--- projects/lesson17/lesson17.ts
+++ projects/lesson17/lesson17.ts
@@ -1,14 +1,28 @@
-function handleException(errorMessage: string): never {
-    throw Error(errorMessage)
-}
+// function handleException(errorMessage: string): never {
+//     throw Error(errorMessage)
+// }
+
+// function runInfinity(): void {
+//     // while (true) {
+//     //     // console.log("running...")
+//     // }
+// }
+
+// // handleException("just a test error");
+
+// let a = handleException("just a test error");
+// console.log(">> check a= ", a)
+
+// function handleException(errorMessage: string): never {
+//     throw Error(errorMessage);
+// }
+// handleException("Just testing error!");
 
 function runInfinity(): void {
     // while (true) {
-    //     // console.log("running...")
+    //     console.log("running...");
     // }
 }
 
-// handleException("just a test error");
-
-let a = handleException("just a test error");
-console.log(">> check a= ", a)
+let a = runInfinity();
+console.log("🚀CHECK  a =>", a);
