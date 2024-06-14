6b2868ef14ce54bbc9251442715d1524106d1e89
kiethohoai <kietho.060193@gmail.com>
2024-06-14 16:59:03 +0700

    22 - TypeScript For Loop

--- chapter3/chapter3.ts
+++ chapter3/chapter3.ts
@@ -1,16 +1,4 @@
-////////////// #21 TypeScript Switch Case ///////////////
-const myAge: number = 30;
-
-switch (myAge) {
-    case 20:
-        console.log("You're 20 years old!");
-        break;
-    case 25:
-        console.log("You're 25 years old!");
-        break;
-    case 30:
-        console.log("You're 30 years old!");
-        break;
-    default:
-        console.log("How old are you?");
+////////////// 22 - TypeScript For Loop //////////////
+for (let i = 0; i < 10; i++) {
+    console.log("🚀CHECK  i =>", i);
 }
