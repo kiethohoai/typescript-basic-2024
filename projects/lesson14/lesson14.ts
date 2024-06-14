78fde851a054f76c52cb12e2d0da7e8290a1c856
kiethohoai <kietho.060193@gmail.com>
2024-06-14 13:36:34 +0700

    14 - Enum Types

--- projects/lesson14/lesson14.ts
+++ projects/lesson14/lesson14.ts
@@ -1,14 +1,23 @@
-enum API_STATUS {
-    PENDDING,
-    FULFILLED = "FULFILLED asfdasfasf",
-    REJECTED = "FULFILLED" //PROMISE
+// enum API_STATUS {
+//     PENDDING,
+//     FULFILLED,
+//     REJECTED,
+// }
+// console.log("🚀CHECK  API_STATUS =>", API_STATUS);
+
+// let data1 = API_STATUS.PENDDING;
+// let data2 = API_STATUS.FULFILLED;
+// let data3 = API_STATUS.REJECTED;
+// console.log("🚀CHECK  data1 =>", data1);
+// console.log("🚀CHECK  data2 =>", data2);
+// console.log("🚀CHECK  data3 =>", data3);
+
+enum UPLOAD_STATUS {
+    UPLOADING = "UPLOADING",
+    PROCESSING = "PROCESSING",
+    PUBLISH = "PUBLISH",
 }
-
-//constant
-//java
-
-let a1 = API_STATUS.PENDDING;
-let a2 = API_STATUS.FULFILLED;
-
-//frontend : syntax
-console.log(">>> a1 = ", a1, " a2 = ", a2)
+console.log("🚀CHECK  UPLOAD_STATUS =>", UPLOAD_STATUS);
+console.log("🚀CHECK  UPLOAD_STATUS.UPLOADING =>", UPLOAD_STATUS.UPLOADING);
+console.log("🚀CHECK  UPLOAD_STATUS.PROCESSING =>", UPLOAD_STATUS.PROCESSING);
+console.log("🚀CHECK  UPLOAD_STATUS.PUBLISH =>", UPLOAD_STATUS.PUBLISH);
