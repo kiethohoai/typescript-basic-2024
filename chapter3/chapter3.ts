d41fd53039c0ede771bce38640ed4551b369c557
kiethohoai <kietho.060193@gmail.com>
2024-06-15 09:54:57 +0700

    31 TypeScript Function Overloading

--- chapter3/chapter3.ts
+++ chapter3/chapter3.ts
@@ -1,40 +1,7 @@
-//////////////// 31 TypeScript Rest Parameters //////////////
+//////////////// 31 TypeScript Function Overloading //////////////
 
-//// #1
-// function getTotal(...numbers: number[]): number {
-//     let total = 0;
-//     numbers.forEach((num) => (total += num));
-//     return total;
-// }
+function Data(a: any, b: any) {
+    return a + b;
+}
 
-// // getTotal(10, 20, 30);
-// console.log("🚀CHECK  getTotal(1, 2, 3) =>", getTotal(1, 2, 3));
-// console.log("🚀CHECK  getTotal(10, 20, 30) =>", getTotal(10, 20, 30));
-
-//// #2
-// function getTotal(...numbers: number[]): number {
-//     let total = 0;
-//     numbers.map((num) => (total += num));
-//     return total;
-// }
-
-// // getTotal(10, 20, 30);
-// console.log("🚀CHECK  getTotal(1, 2, 3) =>", getTotal(1, 2, 3));
-// console.log("🚀CHECK  getTotal(10, 20, 30) =>", getTotal(10, 20, 30));
-
-//// #3
-// function nhan(n: number, ...arr: number[]) {
-//     return arr.map((a) => n * a);
-// }
-// console.log("🚀CHECK  nhan(10, 1, 2, 3, 4) =>", nhan(10, 1, 2, 3, 4));
-// console.log("🚀CHECK  nhan(10, 10, 20, 30, 40) =>", nhan(10, 10, 20, 30, 40));
-
-//// #4
-
-const temp = (fname: string, ...lname: string[]): string => {
-    // return fname + " " + lname.join(" ");
-    return `${fname} ${lname.join(" ")}`;
-};
-
-// temp("Ho", "Hoai", "Kiet");
-console.log("🚀CHECK  temp = ", temp("Ho", "Hoai", "Kiet"));
+console.log("🚀CHECK  Data =>", Data(10, 90), Data("10", true));
